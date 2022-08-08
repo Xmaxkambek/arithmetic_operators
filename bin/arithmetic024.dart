774d176cf0276d601fc6de3b9e55d164e0ff2d1f
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-08 11:53:33 +0500

    024

--- bin/arithmetic024.dart
+++ bin/arithmetic024.dart
@@ -1,4 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+num func(int x, int v) {
+  num c = (2 * x) * (x + v);
+  return c;
+}
+
+void main() {
+  print(func(2, 3));
+}
