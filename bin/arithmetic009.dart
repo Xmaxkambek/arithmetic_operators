9dd7a99598e657db32df08d73c063bd85cf1c882
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-08 11:42:56 +0500

    009

--- bin/arithmetic009.dart
+++ bin/arithmetic009.dart
@@ -1,4 +1,13 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+num func(int c, int b) {
+  num s = pow(c, 2) + pow(b, 2);
+  return s;
+}
+
+void main() {
+  print(func(1, 5));
+}
