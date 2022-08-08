4a7afb00c1e61370455b91539745995bde9f272f
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-08 11:55:11 +0500

    026

--- bin/arithmetic026.dart
+++ bin/arithmetic026.dart
@@ -1,4 +1,14 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+num func(int x) {
+  num c = (x / 2) * (x + pow(x, 2)) / pow(2, x);
+
+  return c;
+}
+
+void main() {
+  print(func(2));
+}
